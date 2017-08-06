2220f44a41e7c67513bb6ad57ab780b14d584eb6
pro-metheus <ajayajith19@gmail.com>
2017-08-06 16:31:09 +0530

    added new header file for readability

--- scheduling_algorithms.c
+++ scheduling_algorithms.c
@@ -1,169 +1,8 @@
+#include"scheduling_algorithms.h"
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-//config settings
-#define NAME_LENGTH 10
-//
-
-
-struct task{
-	int burst_time;
-	char process_name[NAME_LENGTH];
-	int priority;
-	struct task* next;
-}*head=NULL,*new_task,*cur_task;
-
-
-struct shortq{
-		int burst;
-		struct task* link;
-	};
-	
-	
-struct prioq{
-	int prio;
-	struct task* link;
-};
-	
-int count=0;
-
-//thats the fucking data structure we are gonna use
-void spawn_task(char name[NAME_LENGTH],int burst, int priority){
-	new_task=(struct task*)malloc(sizeof(struct task));
-	new_task->burst_time=burst;
-	//printf("%d",burst);
-	new_task->priority=priority;
-	strcpy(new_task->process_name,name);
-	//printf("%d",new_task->burst_time);
-	if(head==NULL){
-		head=new_task;
-		cur_task=new_task;
-	}
-	else{
-		cur_task->next=new_task;
-		cur_task=new_task;
-	}
-
-}//spawn_tasks ends here
-
-
-void view_tasks(){
-	cur_task=head;
-	do{
-		printf("%s has %d burst time \n",cur_task->process_name,cur_task->burst_time);
-		cur_task=cur_task->next;
-	}while(cur_task !=NULL);
-}//view tasks ends here
-
-
-
-
-void fcfs(){
-	int waiting_time;
-	int turn_around_time;
-	int temp;
-	int elapsed=0;
-	cur_task=head;
-	do{
-	temp=cur_task->burst_time;
-	printf("%s task waiting time= %d, turaround time= %d",cur_task->process_name,elapsed,elapsed+temp);
-	elapsed+=temp;
-	cur_task=cur_task->next;		
-	}while(cur_task != NULL);
-}
-
-
-void sjf(){
-	
-	
-	struct shortq list[count];
-	cur_task=head;
-	int i=0;
-	while(cur_task!=NULL){
-		list[i].burst=cur_task->burst_time;
-		list[i].link=cur_task;
-		i++;
-		cur_task=cur_task->next;
-	}  //fills the shortq...now sort it some fucking way im using bubble sort cause i dont give a shit about the fucking complexity 
-	
-	int j=0;
-	i=0;
-	for(i=0;i<count;i++){
-		for(j=0;j<count;j++){
-			if(list[i].burst<list[j].burst){
-				struct shortq temp;
-				temp.burst=list[j].burst;
-				temp.link=list[j].link;
-				list[j].burst=list[i].burst;
-				list[j].link=list[i].link;
-				list[i].burst=temp.burst;
-				list[i].link=temp.link;
-			}
-		}
-	}
-	int elapsed=0;
-	for(i=0;i<count;i++){
-		
-		int temp1;
-		temp1=list[i].link->burst_time;
-		printf("%s task waiting time= %d, turaround time= %d\n",list[i].link->process_name,elapsed,elapsed+temp1);
-		elapsed+=temp1;
-	}
-	
-	
-}
-
-
-
-
-void priority(){
-
-struct prioq queue[count];
-cur_task=head;
-int i=0;
-while(cur_task!=NULL){
-	queue[i].prio=cur_task->priority;
-	queue[i].link=cur_task;
-	i++;
-	cur_task=cur_task->next;
-}//queue filed
-int j;
-
-for(i=0;i<count;i++){
-		for(j=0;j<count;j++){
-			if(queue[i].prio<queue[j].prio){
-				struct prioq temp;
-				temp.prio=queue[j].prio;
-				temp.link=queue[j].link;
-				queue[j].prio=queue[i].prio;
-				queue[j].link=queue[i].link;
-				queue[i].prio=temp.prio;
-				queue[i].link=temp.link;
-			}
-		}
-	}
-	
-	int elapsed=0;
-	for(i=0;i<count;i++){
-		
-		int temp1;
-		temp1=queue[i].link->burst_time;
-		printf("%s task waiting time= %d, turaround time= %d\n",queue[i].link->process_name,elapsed,elapsed+temp1);
-		elapsed+=temp1;
-	}
-
-
-
-}
-
-
-
-
-
-
 void main(){
 	//here lies the main fuction....
-	printf("1 create task, 2 print tasks, 3 fcfs, 4 sjf 5 priority algo 6quit");
+	printf("1 create task, 2 print tasks, 3 fcfs, 4 sjf 5 priority algo 6round robin 7quit");
 	int op;
 	while(1==1){
 	scanf("%d",&op);
@@ -180,15 +19,18 @@ void main(){
 			spawn_task(name,bt,prio);
 			count++;
 			break;
-		case 2:view_tasks();
+		case 2:view_tasks(); //verified
 			break;
-		case 3:fcfs();
+		case 3:fcfs();       //verified
 			break;
-		case 4: sjf();
+		case 4: sjf();      //verified
 			break;
-		case 5: priority();
+		case 5: priority();  //verified
 			break;
-		case 6:exit(0);
+		case 6: round_robin(); //waiting time buggy
+			break;
+		case 7:clear_memory();
+		exit(0);
 	}
 	
 	
---
+++ scheduling_algorithms.h
@@ -0,0 +1,258 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//config settings
+#define NAME_LENGTH 10
+//
+
+
+struct task{
+	int burst_time;
+	char process_name[NAME_LENGTH];
+	int priority;
+	struct task* next;
+}*head=NULL,*new_task,*cur_task,*prev;
+
+
+struct shortq{
+		int burst;
+		struct task* link;
+	};
+	
+	
+struct prioq{
+	int prio;
+	struct task* link;
+};
+	
+struct logger{
+	int spent;
+	int finished;
+	int wait;
+	struct task* link;
+	 
+};
+	
+	
+int count=0;
+
+//thats the fucking data structure we are gonna use
+void spawn_task(char name[NAME_LENGTH],int burst, int priority){
+	new_task=(struct task*)malloc(sizeof(struct task));
+	new_task->burst_time=burst;
+	//printf("%d",burst);
+	new_task->priority=priority;
+	strcpy(new_task->process_name,name);
+	//printf("%d",new_task->burst_time);
+	if(head==NULL){
+		head=new_task;
+		cur_task=new_task;
+	}
+	else{
+		cur_task->next=new_task;
+		cur_task=new_task;
+	}
+
+}//spawn_tasks ends here
+void clear_memory(){
+	cur_task=head;
+			while(cur_task!=NULL){
+				prev=cur_task;
+				free(prev);
+				cur_task=cur_task->next;
+			}
+}
+
+void view_tasks(){
+	cur_task=head;
+	do{
+		printf("%s has %d burst time \n",cur_task->process_name,cur_task->burst_time);
+		cur_task=cur_task->next;
+	}while(cur_task !=NULL);
+}//view tasks ends here
+
+
+
+
+void fcfs(){
+	int waiting_time;
+	int turn_around_time;
+	int temp;
+	int elapsed=0;
+	cur_task=head;
+	do{
+	temp=cur_task->burst_time;
+	printf("%s task waiting time= %d, turaround time= %d",cur_task->process_name,elapsed,elapsed+temp);
+	elapsed+=temp;
+	cur_task=cur_task->next;		
+	}while(cur_task != NULL);
+}
+
+
+void sjf(){
+	
+	
+	struct shortq list[count];
+	cur_task=head;
+	int i=0;
+	while(cur_task!=NULL){
+		list[i].burst=cur_task->burst_time;
+		list[i].link=cur_task;
+		i++;
+		cur_task=cur_task->next;
+	}  //fills the shortq...now sort it some fucking way im using bubble sort cause i dont give a shit about the fucking complexity 
+	
+	int j=0;
+	i=0;
+	for(i=0;i<count;i++){
+		for(j=0;j<count;j++){
+			if(list[i].burst<list[j].burst){
+				struct shortq temp;
+				temp.burst=list[j].burst;
+				temp.link=list[j].link;
+				list[j].burst=list[i].burst;
+				list[j].link=list[i].link;
+				list[i].burst=temp.burst;
+				list[i].link=temp.link;
+			}
+		}
+	}
+	int elapsed=0;
+	for(i=0;i<count;i++){
+		
+		int temp1;
+		temp1=list[i].link->burst_time;
+		printf("%s task waiting time= %d, turaround time= %d\n",list[i].link->process_name,elapsed,elapsed+temp1);
+		elapsed+=temp1;
+	}
+	
+	
+}
+
+
+
+
+void priority(){
+
+struct prioq queue[count];
+cur_task=head;
+int i=0;
+while(cur_task!=NULL){
+	queue[i].prio=cur_task->priority;
+	queue[i].link=cur_task;
+	i++;
+	cur_task=cur_task->next;
+}//queue filed
+int j;
+
+for(i=0;i<count;i++){
+		for(j=0;j<count;j++){
+			if(queue[i].prio<queue[j].prio){
+				struct prioq temp;
+				temp.prio=queue[j].prio;
+				temp.link=queue[j].link;
+				queue[j].prio=queue[i].prio;
+				queue[j].link=queue[i].link;
+				queue[i].prio=temp.prio;
+				queue[i].link=temp.link;
+			}
+		}
+	}
+	
+	int elapsed=0;
+	for(i=0;i<count;i++){
+		
+		int temp1;
+		temp1=queue[i].link->burst_time;
+		printf("%s task waiting time= %d, turaround time= %d\n",queue[i].link->process_name,elapsed,elapsed+temp1);
+		elapsed+=temp1;
+	}
+
+
+
+}
+
+
+void round_robin(){
+	int quanta;
+	struct logger log[count];
+	printf("enter the time quanta :");
+	scanf("%d",&quanta);
+	cur_task=head;
+	int i=0;
+	while(cur_task!=NULL){
+		log[i].finished=0;
+		log[i].link=cur_task;
+		log[i].spent=0;
+		log[i].wait=0;
+		cur_task=cur_task->next;
+		i++;
+	}//inititalized
+	int full=0; //signal for adding waiting time
+	int done=0;
+	while(done ==0){
+		i=0;
+		while(i<count){
+			if(log[i].finished==0){
+			
+				if(log[i].link->burst_time-log[i].spent>=quanta){
+				
+				log[i].spent+=quanta;
+				full=1;
+				}
+				
+				else{
+					log[i].spent+=log[i].link->burst_time-log[i].spent;
+					printf("task %s finished \n",log[i].link->process_name);
+					log[i].finished=1;
+					full=0;
+					
+				}
+
+				
+				}
+				int j;
+				for(j=0;j<count;j++){
+					if(i==j){
+						continue;
+					}
+					else{
+						if(full==1){
+						
+						log[j].wait+=quanta;}
+						else{
+							log[j].wait+=log[i].link->burst_time-log[i].spent;
+						}
+						
+					}
+				}	
+			
+			i++;
+			
+		}
+		int lock=0;
+		int j;
+		for(j=0;j<count;j++){
+			if(log[j].finished==1){
+				lock=1;
+				
+			}
+			else{
+				lock=0;
+				break;
+			}
+		}
+		if(lock==1){
+			done=1;
+		}
+			
+	}
+	for(i=0;i<count;i++){
+		printf("task(%d / %d) %s waited for %d and has a turnarouund time of %d\n",log[i].spent,log[i].link->burst_time,log[i].link->process_name,log[i].wait,log[i].spent+(log[i].wait));
+	}
+}
+
+
+
+
+
